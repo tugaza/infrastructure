28c84709066267ecba00a6230e2dac66d135c5c5
pra <pra@mbarysas-t590>
2021-09-05 21:13:30 +0300

    provide up script for easier startup

---
+++ up.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+mkdir -p /tmp/volatile && chmod 777 /tmp/volatile
+sudo modprobe snd-aloop
+arecord -l | grep Loopback
+if [[ ! -f ../player/vars/environment.sh ]];
+then
+    cp ../player/vars/environment.sh.dist ../player/vars/environment.sh
+    echo "PLAYER ENVIRONMENT UNSET, COPIED FROM .dist; PLEASE REVIEW ENVIRONMENT CONFIGURATION and press <enter> to continue"
+    read
+fi
+if [[ ! -f ../gui/vars/environment.sh ]];
+then
+    cp ../gui/vars/environment.sh.dist ../player/vars/environment.sh
+    echo "PLAYER ENVIRONMENT UNSET, COPIED FROM .dist; PLEASE REVIEW ENVIRONMENT CONFIGURATION and press <enter> to continue"
+    read
+fi
+
+if [[ ! -f docker-compose.yml ]];
+then
+    cp docker-compose.yml.dist docker-compose.yml
+    echo "DOCKER COMPOSE UNSET, COPIED FROM .dist; PLEASE REVIEW DOCKER COMPOSE CONFIGURATION and press <enter> to continue"
+    read
+fi
+
+docker-compose up --build
